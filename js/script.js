4ba955961597ca40127b479d0dfa0a9332437034
n.hanawa <itxstudyx@gmail.com>
2022-05-31 02:23:13 +0900

    追加

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slide_imgs').slick({
+      dots:true,
+      autoplay:true,
+      arrows: false,
+    autoplaySpeed:5000,
+    centerMode: true,
+    centerPadding: '10%',
+    });
+  });
