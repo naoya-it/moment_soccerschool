78bd5cbbfe10f5ffda4b1e8fa71ff5afa69d9fd4
n.hanawa <itxstudyx@gmail.com>
2022-06-05 11:37:59 +0900

    モーダル作成前

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ $(function(){
     centerPadding: '10%',
     });
   });
+
+  $(function () {
+    $('.js-open').click(function () {
+      $('#overlay, .modal-window').fadeIn();
+    });
+    $('.js-close').click(function () {
+      $('#overlay, .modal-window').fadeOut();
+    });
+  });
